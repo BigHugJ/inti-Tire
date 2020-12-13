3e0fa2f9d9003e956786ccfdf26bd6e4d589a0c3
junlugrace <junlu05@gmail.com>
2020-12-13 14:45:40 -0500

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -1,42 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-const TableHeader = () => {
+const TableHeader = () => { 
   return (
     <thead>
       <tr>
-        <th>Names</th>
+        <th>Name</th>
         <th>Job</th>
       </tr>
     </thead>
-  )
+  );
 }
 
-const TableBody = (props) => {
-  const rows = props.characterData.map((row, index) => {
+const TableBody = props => { 
+  const rows = props.messagesData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
-        <td>
-          <button onClick={() => props.removeCharacter(index)}>Delete</button>
-        </td>
+        <td>{row.message}</td>
+        <td>{row.sender}</td>
+        <td><button onClick={() => props.removeMessage(index)}>Del</button></td>
       </tr>
-    )
-  })
+    );
+  });
 
-  return <tbody>{rows}</tbody>
+  return <tbody>{rows}</tbody>;
 }
 
 const Table = (props) => {
-  const {characterData, removeCharacter} = props
-
+  const { messagesData, removeMessage } = props;
   return (
     <table>
-      <TableHeader />
-      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+      <TableBody messagesData={messagesData} removeMessage={removeMessage} />
     </table>
   )
 }
 
-
-export default Table
+export default Table;
