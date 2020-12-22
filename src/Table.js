b8fa0cbdfbe268b22825a5b127a0d6d214d2cb2e
J&J <junlu05@gmail.com>
2020-12-22 18:07:08 -0500

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -1,35 +1,21 @@
 import React from 'react';
 import MessageCard from './MessageCard'
-
-const TableHeader = () => { 
-  return (
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Job</th>
-      </tr>
-    </thead>
-  );
-}
+import {Container, Row, Col, Accordion} from 'react-bootstrap' 
 
 const TableBody = props => { 
   const rows = props.messagesData.map((row, index) => {
     return (
-      <tr key={index}>
-        <td><MessageCard message={row.message}/></td>
-      </tr>
+	  <MessageCard message={row.message} eventIndex={index}/>
     );
   });
 
-  return <tbody>{rows}</tbody>;
+  return <Accordion>{rows}</Accordion>;
 }
 
 const Table = (props) => {
   const { messagesData, removeMessage } = props;
   return (
-    <table>
-      <TableBody messagesData={messagesData} removeMessage={removeMessage} />
-    </table>
+    <TableBody messagesData={messagesData} removeMessage={removeMessage} />  
   )
 }
 
