ab300792b4babbc05a708782c537deb76e034692
J&J <junlu05@gmail.com>
2020-12-22 18:06:24 -0500

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react'
 import Table from './Table'
 import Form from './Form'
 import Example from './Example'
-import MessageCard from './MessageCard';
+import MessageCard from './MessageCard'
+import {Container, Button, Jumbotron, Row, Col} from 'react-bootstrap'
+import Counters from './Counter'
 
 class Chat extends Component {
   state = {
@@ -27,15 +29,33 @@ class Chat extends Component {
     const { messages } = this.state;
 
 	  return (
-	    <div className="container">
-        <h1>Chatting</h1>
-        <Example />
-        <Table
-          messagesData={messages}
-          removeMessage={this.removeMessage}
-        />
-        <Form handleSubmit={this.handleSubmit} />
-      </div>
+	    <Container >
+		  <Jumbotron >
+		    <Row >
+			  <Col>
+				<h1>Chatting</h1>
+			  </Col>
+			</Row>
+			<Row className="mb-2">
+			  <Col>
+				<Counters unreaded = '10' totalMessages = '11'/>
+			  </Col>
+			</Row>
+			<Row className="mb-5">
+			  <Col>
+				<Form handleSubmit={this.handleSubmit} />
+			  </Col>
+			</Row>
+			<Row className="mb-1">
+			  <Col>
+				<Table
+				  messagesData={messages}
+				  removeMessage={this.removeMessage}
+				/>
+			  </Col>
+			</Row>
+		  </Jumbotron>
+        </Container>
 	  )
   }
 }
