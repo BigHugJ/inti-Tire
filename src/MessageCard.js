9172fe4a50d4e600ae324489b1b716a6aee74975
J&J <junlu05@gmail.com>
2020-12-22 18:07:56 -0500

    Update MessageCard.js

--- src/MessageCard.js
+++ src/MessageCard.js
@@ -1,40 +1,34 @@
 import Toast from 'react-bootstrap/Toast';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
-import {useState} from 'react'
-import { Container } from 'react-bootstrap';
+import { Container, Accordion, Card } from 'react-bootstrap'
+import {useAccordionToggle} from 'react-bootstrap'
 
-function MessageToast (props) {
-    const [show, setShow] = useState(false);
-    const message = props.message;
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionToggle(eventKey, () =>
+    console.log('totally custom!'),
+  );
+  return (
+    <Accordion.Toggle onClick={decoratedOnClick}>
+      {children}
+    </Accordion.Toggle>
+  );
+}
 
+function MCard (props) {
+	const message = props.message;
+	const eventKey = props.eventIndex
+	
     return (
-    <Container>
-      <Row>
-        <Col xs={236}>
-          <Toast onClose={() => setShow(false)} show={show} delay={4000} autohide>
-            <Toast.Header>
-              <img
-                src="holder.js/20x20?text=%20"
-                className="rounded mr-2"
-                alt=""
-              />
-              <strong >f-message</strong>
-              <small>{new Date().toLocaleString()}</small>
-            </Toast.Header>
-            <Toast.Body>{message}</Toast.Body>
-          </Toast>
-        </Col>
-        <Col xs={16}>
-          <Button onClick={() => setShow(true)}>check</Button>
-        </Col>
-      </Row>
-      </Container>
+      <Card>
+        <CustomToggle eventKey={eventKey}>hbh</CustomToggle>
+        <Accordion.Collapse eventKey={eventKey}>
+          <Card.Body>{message}</Card.Body>
+        </Accordion.Collapse>
+      </Card>
     );
 }
 
 const MessageCard = (props) => {
-    return (<MessageToast message={props.message}/>)
+    return (<MCard message={props.message} eventIndex={props.eventIndex}/>)
 }
 export default MessageCard;
