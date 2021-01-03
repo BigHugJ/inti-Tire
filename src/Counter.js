612755cdd8ae9d46b704f3e776cb954ce291e02b
J&J <junlu05@gmail.com>
2021-01-03 17:30:37 -0500

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -3,10 +3,11 @@ import Badge from 'react-bootstrap/Badge'
 function CounterBadge (props) {
 	const User = props.loginUser
 	const totalMessages = props.totalMessages
+	const isLoggedIn = props.isLoggedIn
 	return (
 		<div>
 		  <Badge pill variant="primary">Total Messages: {totalMessages}</Badge>{' '}
-		  <Badge pill variant="danger">You: {User}</Badge>{' '}
+		  <Badge pill variant={isLoggedIn?"primary":"danger"}>You: {User}</Badge>{' '}
 		</div>
 	)
 }
