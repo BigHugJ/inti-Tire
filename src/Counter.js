8b0b8b46e2bc5d99fae1dd388bc4234a1c3ad00b
J&J <junlu05@gmail.com>
2021-01-04 17:55:29 -0500

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,31 @@
 import Badge from 'react-bootstrap/Badge'
+import console from "react-console"
 
 function CounterBadge (props) {
 	const User = props.loginUser
 	const totalMessages = props.totalMessages
 	const isLoggedIn = props.isLoggedIn
-	return (
+	if (isLoggedIn)
+	  return (
 		<div>
 		  <Badge pill variant="primary">Total Messages: {totalMessages}</Badge>{' '}
-		  <Badge pill variant={isLoggedIn?"primary":"danger"}>You: {User}</Badge>{' '}
+			<Badge pill variant="primary">You: {User}</Badge>
 		</div>
-	)
+	  )
+	else 
+	  return (
+		<div>
+		  <Badge pill variant="primary">Total Messages: {totalMessages}</Badge>{' '}
+			<Badge pill variant="danger">You: {User}</Badge>
+		</div>
+	  )
+		
 }
 
 const Counters = (props) => {
-  const { totalMessages, loginUser } = props;
+  const { totalMessages, loginUser, isLoggedIn } = props;
   return (
-	<CounterBadge totalMessages={totalMessages} loginUser={loginUser} />
+	<CounterBadge totalMessages={totalMessages} loginUser={loginUser} isLoggedIn={isLoggedIn} />
   )
 }
 export default Counters;
