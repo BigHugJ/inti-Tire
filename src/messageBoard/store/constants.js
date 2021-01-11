a47288b743886228fbb5a276c063b75e4d23c8c2
caicai43 <173654743@qq.com>
2021-01-11 16:42:43 -0500

    test only

--- src/messageBoard/store/constants.js
+++ src/messageBoard/store/constants.js
@@ -1,4 +1,4 @@
 export const CHANGE_INPUT_VALUE = 'change_input_value'
 export const ADD_MESSAGE_LIST = 'add_message_list'
 export const DELETE_MESSAGE = 'delete_message'
-export const INIT_LIST = 'init_list'
+export const INIT_LIST = 'init_list' 
