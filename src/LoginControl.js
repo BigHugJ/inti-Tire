5316d6f979ad4fc2166221052cd21cbb54842c32
Jun Lu <junlu@Juns-iMac.local>
2021-01-16 09:32:40 -0500

    changed

--- src/LoginControl.js
+++ src/LoginControl.js
@@ -7,24 +7,17 @@ import Image from 'react-bootstrap/Image'
 import SockJS from "sockjs-client"
 import Stomp from "stompjs"
 
-var url = 'http://192.168.2.27:8080/gs-guide-websocket';
-var options = {
-	host: '192.168.2.27',
-	oringin: '192.168.2.35'
-};
-var sockjs = new SockJS(url, /*_reserved*/null, options);
-var stompClient = Stomp.over(sockjs);
 
 function LoginButton(props) {
 	return (
 		<Container className="header">
 			<Jumbotron >
 				<h1>鸿雁之笺</h1>
-				<Button size="sm" type="input" onClick={props.onClick('wolf')}>
+				<Button size="sm" type="input"  >
 					<Image src="wolf.jpg" alt="WOLF" width="80px" height="80px" rounded />
 				</Button>
 				{" "}
-				<Button size="sm" type="input" onClick={props.onClick('bunny')}>
+				<Button size="sm" type="input" >
 					<Image src="bunny.jpg" alt="BUNNY" width="80px" height="80px" rounded />
 				</Button>
 			</Jumbotron>
@@ -38,9 +31,10 @@ class LoginControl extends React.Component {
 		loginUser: ''
 	};
 
-	handleLoginClick = user => () => {
+	handleLoginClick = () => {
 		console.log("connecting...")
-		stompClient.connect({}, this.connectionSuccess(user));
+		this.setState({ isLoggedIn: true });
+		this.setState({ loginUser: '' });
 	}
 
 	connectionSuccess(user) {
@@ -49,32 +43,13 @@ class LoginControl extends React.Component {
 		this.setState({ isLoggedIn: true });
 		this.setState({ loginUser: user });
 
-		stompClient.subscribe('/topic/javainuse', this.onMessageReceived);
-
-		stompClient.send("/app/chat.newUser", {}, JSON.stringify({
-			messageSender: user,
-			message: 'first connection',
-		}))
+		
 	}
 
-
-	onMessageReceived = payload => () => {
-		var respMessage = JSON.parse(payload.body);
-		console.log(respMessage.message)
-	}
 
 	sendMessage = chatMsg => () => {
 		console.log(chatMsg)
-		if (chatMsg && stompClient) {
-			var chatMessage = {
-				messageSender : this.state.user,
-				messageReceiver: 'you',
-				message : chatMsg
-			};
-	
-			stompClient.send("/app/chat.sendMessage", {}, JSON
-					.stringify(chatMessage));
-		}
+		
 	}
 
 	render() {
