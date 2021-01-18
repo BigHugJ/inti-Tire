c313e51281a6e585ce1cfeb222934f0dc02bd629
J&J <junluy@hotmail.com>
2021-01-18 08:55:58 -0500

    Create SockConstants.js

---
+++ src/SockConstants.js
@@ -0,0 +1,8 @@
+export const _ADD_NEWUSER = 'added'
+export const _CHAT_REQUEST = 'chating'
+export const _STATUS_ONLINE = 'online'
+export const _SOCK_SERVER = 'http://192.168.2.27:8080/gs-guide-websocket'
+export const _CHAT_TOPIC_PREFIX = '/topic/jj'
+export const _MESSAGE_DEST_ADDUSER = '/app/chatNewUser'
+export const _MESSAGE_DEST_SENDMSG = '/app/chatSendMessage'
+export const _SOCKET_READY = 1
