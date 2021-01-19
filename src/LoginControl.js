a33122caa280632365a750b69b1f7ce51bcd9107
caicai43 <173654743@qq.com>
2021-01-19 09:56:45 -0500

    add login page

--- src/LoginControl.js
+++ src/LoginControl.js
@@ -1,58 +1,102 @@
 import React from 'react'
-import Chat from './Chat';
-import { Container, Button, Jumbotron } from 'react-bootstrap'
+import { Container, Button, Jumbotron,InputGroup,FormControl,Card } from 'react-bootstrap'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Image from 'react-bootstrap/Image'
+import MainTab from './MainTab'
+import axios from 'axios';
+
+
 
 class LoginControl extends React.Component {
   constructor(props) {
 	super(props)
     this.state = {
 	  isLoggedIn: false,
-	  loginUser: ''
-    }
+	  loginUser: '',
+	  show:false,
+	  list:[],
+	  loginUserAvatar:''
+	  
+	}
   }
-
-  clickWolf = () => {
-	console.log("click wolf")
-
-	this.setState({ isLoggedIn: true });
-	this.setState({ loginUser: "wolf" });  
-	
+  
+  handleClick(username,avatar){
+		this.setState({ loginUser: username,
+						show: true, 
+						loginUserAvatar:avatar })  
+	  }
+  
+  handleLogin(){
+	  this.setState({ isLoggedIn: true });
   }
-  clickBunny = () => {
-	console.log("click wolf")
-
-	this.setState({ isLoggedIn: true });
-	this.setState({ loginUser: "bunny" });  
-	
+  hideLoginBox(){
+	  this.setState({ show: false });  
+	  
   }
+ 
+  loginPanel ()  {
+	  return (
+		  <div  style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+	      <Card style={{ width: '30rem' }}  >
+	      	<Card.Body>
+	      		<button type="button" class="close" aria-label="Close" onClick={() => this.hideLoginBox()} >
+	      			<span aria-hidden="true">&times;</span>
+	      		</button>  
+	      		<Image src={this.state.loginUserAvatar} alt="BUNNY" width="80px" height="80px" rounded />
+	      		<p/> 
+	      		<InputGroup className="mr-sm-2">
+			      	<InputGroup.Prepend>
+			      		<InputGroup.Text>user name</InputGroup.Text>
+			      	</InputGroup.Prepend>
+			      	<FormControl  className="mr-sm-2"/>
+			    </InputGroup>
+			    <br/>
+			    <InputGroup className="mr-sm-2">
+			    	<InputGroup.Prepend>
+			      		<InputGroup.Text>password</InputGroup.Text>
+			      	</InputGroup.Prepend>
+			      	<FormControl  type="password" className="mr-sm-2"/>
+			    </InputGroup>
+			    <br/>
+			    <Button variant="info" onClick={()=>this.handleLogin()}>Login</Button>	
+			 </Card.Body>
+		  </Card>
+	  </div>
+)}
+  
+ 
+  componentDidMount(){
+	  axios.get('../data/UserData.json',{headers:{'content-type':'application/x-www-form-urlencoded'}}).then((res)=>{
+		  this.setState({ list: res.data });  
+		})
+	}
+
   
   render() {
     const isLoggedIn = this.state.isLoggedIn
 	const loginUser = this.state.loginUser
 
-	console.log("okok")
 
 	if (this.state.isLoggedIn === false) {
 	  return (
 		<Container className="header">
 	      <Jumbotron >
 		    <h1>鸿雁之笺</h1>
-		    <Button size="sm" type="input" onClick={this.clickWolf} >
-		      <Image src="wolf.jpg" alt="WOLF" width="80px" height="80px" rounded />
-		    </Button>
-		    {" "}
-		    <Button size="sm" type="input" onClick={this.clickBunny}>
-		      <Image src="bunny.jpg" alt="BUNNY" width="80px" height="80px" rounded />
-		    </Button>
+		    {this.state.list.map((item,index)=>{return  <div style= {{display : 'inline-block',padding:'20px'}}>
+		    <Button size="sm" type="input" onClick={(e)=>this.handleClick(item.username,item.avatar)} >
+		      <Image src={item.avatar} alt={item.username} width="80px" height="80px" rounded />
+		    </Button> </div>
+	    })}
 	      </Jumbotron>
+	      {this.state.show === true? this.loginPanel():null}
+	      
         </Container>
       )
 	}
 	else {
-	  return <Chat loginUser={loginUser} isLoggedIn={isLoggedIn} sendMessage={this.sendMessage} />
+		console.log(this.state.loginUser);
+		return <MainTab loginUser={loginUser} isLoggedIn={isLoggedIn} sendMessage={this.sendMessage} />
 	}
   }
 }
