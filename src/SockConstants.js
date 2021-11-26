bb4316b7651aca1883970ffb05c5319ba0c32830
J&J <junluy@hotmail.com>
2021-11-25 21:11:53 -0500

    jj

--- src/SockConstants.js
+++ src/SockConstants.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const _ADD_NEWUSER = 'added'
 export const _CHAT_REQUEST = 'chatting'
 export const _STATUS_ONLINE = 'online'
@@ -13,23 +12,4 @@ export const _MESSAGE_DEST_ADDCHANNEL = '/app/chatChannel'
 export const _MESSAGE_DEST_DELETEUSER = '/app/chatDeleteUser'
 export const _SOCKET_READY = 1
 export const _SOCKET_CLOSED = 3
-
 export const _MESSAGE__TIME_SPLITTER = 'time_split'
-=======
-export const _ADD_NEWUSER = 'added'
-export const _CHAT_REQUEST = 'chatting'
-export const _STATUS_ONLINE = 'online'
-export const _SOCK_SERVER_EXTERNAL = 'http://74.14.164.223:8080/gs-guide-websocket'
-export const _SOCK_SERVER = 'http://192.168.2.27:8080/gs-guide-websocket'
-
-export const _CHAT_TOPIC_PREFIX = '/topic/jj'
-export const _MESSAGE_DEST_BROADCASTUSERS = '/topic/broadcastUsers'
-export const _MESSAGE_DEST_ADDUSER = '/app/chatNewUser'
-export const _MESSAGE_DEST_SENDMSG = '/app/chatSendMessage'
-export const _MESSAGE_DEST_ADDCHANNEL = '/app/chatChannel'
-export const _MESSAGE_DEST_DELETEUSER = '/app/chatDeleteUser'
-export const _SOCKET_READY = 1
-export const _SOCKET_CLOSED = 3
-
-export const _MESSAGE__TIME_SPLITTER = 'time_split'
->>>>>>> eb78e5efa1ce3d2d7ce9cd265d27973e06859a7d
